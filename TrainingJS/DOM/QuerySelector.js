97a6c1885a3fbc3dfac2a3b10197fdb4ec37c7b3
Lucas C. Bresolino <lucascaladob@gmail.com>
2021-09-17 00:22:45 -0300

    Lucas Add final stage

---
+++ TrainingJS/DOM/QuerySelector.js
@@ -0,0 +1,8 @@
+/*Seleciona apenas os primeiro */
+document.querySelector('.div');
+/*Sleciona qualquer elemento*/
+document.querySelector('#id');
+document.querySelector('tag');
+
+/*Seleciona tudo com oq crusa a pesquisa*/
+document.querySelectorAll('.div');
---
+++ TrainingJS/DOM/SelectElements.js
@@ -0,0 +1,8 @@
+document.getElementById('id');
+/*Seleciona na pagina um elemento com esse id*/
+document.getElementsByClassName("name");
+/*Seleciona um elemento apartir da class dele*/
+document.getElementsByTagName('name');
+/*Seleciona um elemento apartir da tag dele Ex: <img src=""/>*/
+document.getElementsByName("name");
+/*Seleciona apartir do name dele Exe <input name="email"/> */
