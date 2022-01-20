a7d7e0986b5ff55d693cdd67b84bfa259cd96ee4
Lucas C. Bresolino <lucascaladob@gmail.com>
2022-01-20 07:21:04 -0300

    Studie Js

--- TrainingJS/Manipulation/array.js
+++ TrainingJS/Manipulation/array.js
@@ -24,15 +24,78 @@ list.push('Sandwhich');
 list[ list.length + 1 ] ='Atum';
 
 // Remove a item of the array
-list.splice(2,1) // X index . Y how many you want to delete. If Y is null, everything from the index will be deleted 
+list.splice(2,1) // X index . Y how many itens you want to delete. If Y is null, everything from the index will be deleted 
 
 // Concat the arrays
 let eletro = ['Ar-condicionado','Lava-Louças','Batedeira'];
 let tecnologies = ['Tv','Notebook','Smartphone','Hi-fi bluetooch fone'];
-tecnologies.concat(eletro);
+//Need new variable for save the data values
+let All = tecnologies.concat(eletro);
+//See?
+console.log('Tecnologies: '+tecnologies);
+console.log('All: '+All);
 
 // Ordenation in the array
 eletro.sort(); // A - Z
 
 // Reverse the array
-tecnologies.reverse(); 
+tecnologies.reverse();
+
+//Delete item in Arrays
+let delt = ['Lucas','Helena','Lolly'];
+console.log('No-Splice: '+delt);
+
+delt.splice(2,1);
+console.log('Splice: '+delt);
+
+//Map for each value and execute the function x * 2
+let varX = [32,16,8,4,2];
+let vaxY = [];
+varY = varX.map(
+    function(item){
+        return item * 2;
+    }    
+);
+console.log('varY: '+varY);
+
+//Execute a filter in the array and save values in varY
+varY = varX.filter(
+    function(item){
+        if(item%3==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+);
+console.log('Filter: '+varY);
+
+
+//If all is true return true if one our more is false retunr false
+let varS = varX.every(
+    function(item){
+        if(item > 20){
+            return true;
+        }else{
+            return false;
+        }
+    }
+);
+console.log('Every: '+varS);
+
+//Is someone is more big than 20 return true, if not return false 
+varS = varX.some(
+    function(item){
+        if(item > 20){
+            return true;
+        }else{
+            return false;
+        }
+    }
+);
+console.log('Some: '+varS);
+
+//resume the function ->
+function(item){
+    return(item>20)? true : false;
+}
