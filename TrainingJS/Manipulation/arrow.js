975b454011f9116fde1dd75f6c331f6aa785c090
Lucas C. Bresolino <lucascaladob@gmail.com>
2022-02-10 15:43:30 -0300

    .

---
+++ TrainingJS/Manipulation/arrow.js
@@ -0,0 +1,10 @@
+function somarFunc(){
+    return x + y;
+}
+let somarVar = function(){
+    return x + y;
+}
+const somarArrow = (x,y)=>{
+    return x + y;
+}
+const somarArrowOn = (x,y) => x + y;
