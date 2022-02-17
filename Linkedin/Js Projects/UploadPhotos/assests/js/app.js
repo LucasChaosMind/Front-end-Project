88ee504dd8f1d1d163ed0677ded4d0e7ad538d5c
Lucas C. Bresolino <lucascaladob@gmail.com>
2022-02-17 00:10:41 -0300

    html and CSS finish

--- Linkedin/Js Projects/UploadPhotos/assests/js/app.js
+++ Linkedin/Js Projects/UploadPhotos/assests/js/app.js
@@ -2,3 +2,20 @@ function init(){
     let up = document.querySelector('#uploadPhoto');
     up.click();
 }
+function startUp(){
+    let file = document.querySelector('#uploadPhoto').files;
+    console.log(file.length);
+    
+    let url = URL.createObjectURL(file[0]);
+    console.log(url);
+    
+    let title = document.querySelector('#photo-title').value;
+    console.log(title);
+
+    let desc = document.querySelector('')
+
+    html = ``;
+    
+    document.getElementById('area').appendChild(img);
+    
+}
