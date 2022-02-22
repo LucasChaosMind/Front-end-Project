9f4c6331df3c69a9fa60ab826d81b336fdf85afd
Lucas C. Bresolino <lucascaladob@gmail.com>
2022-02-22 16:52:50 -0300

    quase pronto

--- TrainingJS/Requsitions/promisse.js
+++ TrainingJS/Requsitions/promisse.js
@@ -1,7 +1,7 @@
-function temperatureIndice(){
-    return new Promise(function(resolve,reject){
+function temperatureIndice() {
+    return new Promise(function (resolve, reject) {
 
-        setTimeout(function(){
+        setTimeout(function () {
             resolve('40 na sombra');
         }, 2000);
     });
@@ -9,10 +9,57 @@ function temperatureIndice(){
 
 let temp = temperatureIndice();
 
-temp.then(function(resultado){
-    console.log('Temperatura: '+resultado)
+temp.then(function (resultado) {
+    console.log('Temperatura: ' + resultado)
 });
-temp.catch(function(){
+temp.catch(function () {
     console.log('Error!')
 });
 console.log(temp);
+
+//Criando Promise
+
+let myPromise = new Promise(() => {
+    let name = 'ChaosMind';
+
+    if (nome == 'ChaosMind') {
+        resolve('OK!');
+    } else {
+        reject('Error');
+    }
+});
+myPromise.then((data) => {
+    console.log(data);
+});
+
+// Encadeamento de then's
+
+let MyPromise = new Promise(() => {
+    let name = 'ChaosMind';
+
+    if (nome == 'ChaosMind') {
+        resolve('OK!');
+    } else {
+        reject('Error');
+    }
+});
+
+MyPromise.then((data) => {
+    console.log(data);
+}).catch((e) => {
+    console.log('\nError: ' + e);
+})
+
+//Resolver varias promesssas
+
+proA = new Promise((resolve,reject) =>{
+    resolve('Promise 1 Ok');
+});
+proB = new Promise((resolve,reject) =>{
+    resolve('Promise 2 Ok');
+});
+
+//Assoncrina ->
+let resolveAll = Promise.all([proA, proB]).then((data) =>{
+    console.log('Work! '+data);
+})
